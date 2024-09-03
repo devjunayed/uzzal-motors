748ac0a02c60fb214ab7d1b35a0207e551d882f4
Md Junayed <devjunayed@gmail.com>
2024-09-03 15:05:02 +0600

    building

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../globals.css";
 import Navbar from "@/components/homepage/Navbar/Navbar";
 import Footer from "@/components/homepage/Footer/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Uzzal Motors | Home",
@@ -18,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <div>
           <Navbar />
           {children}
--- src/app/api/employee/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(){
-    try {
-        return NextResponse.json({message: "working"});
-    } catch (error: any) {
-        return NextResponse.json({message: error.message});
-    }
-}
